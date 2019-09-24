f1bc2df33457889a86e3ca3866e06019aa8e7e72
sophia_polito <spol5736@uni.sydney.edu.au>
2019-09-25 00:55:42 +1000

    Updates KSS and tiredness scale, added flight questions, added sleep time questions

--- JetSetApp/app/resources/surveyQuestions/BreakfastQuestions.js
+++ JetSetApp/app/resources/surveyQuestions/BreakfastQuestions.js
@@ -1,19 +1,17 @@
 export const survey = [
     {
-        questionType: 'SelectionGroup',
-        questionText:
-            'Please select your flight',
-        questionId: 'flightDetails',
-        options: [
-            {
-                optionText: 'JFK-SYD dep 21:00 from New York; arrive 7:30 Syd time',
-                value: 'JFK'
-            },
-            {
-                optionText: 'LHR-SYD dep 10:00 from London; arrive 17:00 Syd time',
-                value: 'LHR'
-            }
-        ]
+        questionType: 'TimeInput',
+        questionText: 'What time did you fall asleep last night',
+        questionId: 'sleepTime',
+        optional: false,
+        placeholderText: 'Enter time went to sleep',
+    },
+    {
+        questionType: 'TimeInput',
+        questionText: 'What time did you wake up today',
+        questionId: 'awakeTime',
+        optional: false,
+        placeholderText: 'Enter time you woke today',
     },
     {
         questionType: 'TimeInput',
@@ -65,30 +63,42 @@ export const survey = [
             'Please select the number that represents your sleepiness level during the last five minutes',
         questionId: 'kssResult',
         options: [
-            {
-                optionText: '1. Extremely alert',
-                value: '1'
-            },
-            {
-                optionText: '2. Very alert',
-                value: '2'
-            },
-            {
-                optionText: '3. Alert',
-                value: '3'
-            },
-            {
-                optionText: '4. Rather alert ',
-                value: '4'
-            },
-            {
-                optionText: '5. Neither alert nor sleepy ',
-                value: '5'
-            },
-            {
-                optionText: '6. Some signs of sleepiness ',
-                value: '6'
-            }
+          {
+              optionText: '1. Extremely alert',
+              value: '1'
+          },
+          {
+              optionText: '2. Very alert',
+              value: '2'
+          },
+          {
+              optionText: '3. Alert',
+              value: '3'
+          },
+          {
+              optionText: '4. Rather alert ',
+              value: '4'
+          },
+          {
+              optionText: '5. Neither alert nor sleepy ',
+              value: '5'
+          },
+          {
+              optionText: '6. Some signs of sleepiness ',
+              value: '6'
+          },
+          {
+              optionText: '7. Sleepy, but no effort to keep awake ',
+              value: '7'
+          },
+          {
+              optionText: '8. Sleepy, some effort to keep awake ',
+              value: '8'
+          },
+          {
+              optionText: '9. Very sleepy, great effort to keep awake, fighting sleep ',
+              value: '9'
+          }
         ]
     },
     {
@@ -118,8 +128,28 @@ export const survey = [
                 value: '4'
             },
             {
-                optionText: '5 - Very bad jetlag',
+                optionText: '5',
                 value: '5'
+            },
+            {
+                optionText: '6',
+                value: '6'
+            },
+            {
+                optionText: '7',
+                value: '7'
+            },
+            {
+                optionText: '8',
+                value: '8'
+            },
+            {
+                optionText: '9',
+                value: '9'
+            },
+            {
+                optionText: '10 - Very bad jetlag',
+                value: '10'
             }
         ]
     },
--- JetSetApp/app/resources/surveyQuestions/NewUserQuestions.js
+++ JetSetApp/app/resources/surveyQuestions/NewUserQuestions.js
@@ -3,4 +3,20 @@ export const survey = [
         questionType: 'Info',
         questionText: 'Welcome New User'
     },
+    {
+        questionType: 'SelectionGroup',
+        questionText:
+            'Please select your flight',
+        questionId: 'flightDetails',
+        options: [
+            {
+                optionText: 'JFK-SYD dep 21:00 from New York; arrive 7:30 Syd time',
+                value: 'JFK'
+            },
+            {
+                optionText: 'LHR-SYD dep 10:00 from London; arrive 17:00 Syd time',
+                value: 'LHR'
+            }
+        ]
+    }
 ];
