d968ea7cda55be53f0e1b84e912ebd6b51187176
sophia_polito <spol5736@uni.sydney.edu.au>
2019-10-13 18:21:43 +1100

    Added second flight to the flight data

--- JetSetApp/app/resources/FlightData/FlightData.js
+++ JetSetApp/app/resources/FlightData/FlightData.js
@@ -2,8 +2,16 @@ export const flightData = [
     {
         flightCode: 'LHR',
         flightPath: 'London to Sydney',
-        flightTimeStart: '5:36 pm',
-        flightTimeUTCOffset: '+11',
+        flightTimeStart: '10:00 pm',
+        flightTimeUTCOffset: '+1',
+        flightLength: 1380,
+        flightLengthOffset: 180,
+    },
+    {
+        flightCode: 'JFK',
+        flightPath: 'New York to Sydney',
+        flightTimeStart: '9:00 pm',
+        flightTimeUTCOffset: '-4',
         flightLength: 1380,
         flightLengthOffset: 180,
     }
