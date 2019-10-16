2df7a8986aba35d2d43f8efab54d72b123286744
Brendon <blam6948@uni.sydney.edu.au>
2019-10-16 15:55:21 +1300

    Styling for Main Menu done

--- JetSetApp/app/screens/NewUser/NewUser.js
+++ JetSetApp/app/screens/NewUser/NewUser.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import {Text, View, Button, Image} from 'react-native';
 
 import styles from './Styles'
 
@@ -7,10 +7,11 @@ export default class NewUser extends Component {
     static navigationOptions = () => {
         return {
             headerStyle: styles.headerStyle,
-            headerTintColor: '#fff',
-            headerTitle: 'JetSet',
+            headerTintColor: 'black',
+            headerTitle:"Home Page",
             headerTitleStyle: {
                 flex: 1,
+                textAlign: 'center'
             }
         };
     };
@@ -20,8 +21,10 @@ export default class NewUser extends Component {
 
         return (
             <View style={styles.background}>
+                <Image style={{width: 100, height: 100, }} source={require('../../../assets/usydlogo.png')}/>
+                <Text style={{fontWeight: "bold"}}>JetSet Survey App</Text>
                 <View style={styles.container}>
-                    <Button color="#000000"
+                    <Button color="black"
                         onPress={() => navigation.navigate('Survey', { survey: 'NewUserQuestions' })}
                         title="New User"
                     />
